41a7c1274b6b5a55451347b0fb45eae9fc1cbf89
Gautam Nanda <gn567@snu.edu.in>
2023-11-07 18:49:19 +0530

    scss init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  webpack(config) {
+    config.module.rules.forEach((rule) => {
+      const { oneOf } = rule;
+      if (oneOf) {
+        oneOf.forEach((one) => {
+          if (!`${one.issuer?.and}`.includes("_app")) return;
+          one.issuer.and = [path.resolve(__dirname)];
+        });
+      }
+    });
+    return config;
+  },
+  sassOptions: {
+    additionalData: `@import "styles/root/_mixins.scss";`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
